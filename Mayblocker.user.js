0fe33230975dfaee85002f92ebb0c9f731bdaf82
mlobby <58540419+mlobby@users.noreply.github.com>
2019-12-06 08:23:03 -0500

    Inserted extension url

--- Mayblocker.user.js
+++ Mayblocker.user.js
@@ -9,4 +9,4 @@
 // @grant        none
 // ==/UserScript==
 
-window.location.redirect(
+window.location.href("chrome-extension://mlbgnlemejoeocmncpolejmdbimkoiol/block.html?cat=blacklist&url=" + window.location
