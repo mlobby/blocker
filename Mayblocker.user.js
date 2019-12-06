d64fc727c8b6d989020652b3db5021849db9d6dc
mlobby <58540419+mlobby@users.noreply.github.com>
2019-12-05 22:08:57 -0500

    Rename and update basic parts

---
+++ Mayblocker.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Mayblocker
+// @namespace    https://mlobby.github.io/
+// @version      0.1
+// @description  Blocks important sites so you don't have to work.
+// @author       TalonFist
+// @match        https://*.mayfieldschools.org*
+// @match        https://*.schoology.org*
+// @grant        none
+// ==/UserScript==
+
+window.location.redirect(
